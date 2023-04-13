7101132e7e1c7d4cb403320502bab1077bbfad49
niko2205 <87126305+niko2205@users.noreply.github.com>
2023-04-13 21:26:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ from scipy.stats import kstest
 chat_id = 252623629
 
 def solution(x: np.array, y: np.array) -> bool:
-  p_value = kstest(x, y)[1]
+  p_value = kstest(x, y)[0.1]
   return p_value < 0.05
